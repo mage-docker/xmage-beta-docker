db33ced71f16a8a08544cd14fe4bfbcdd22c3466
Goesta <goesta@helta-skelta.de>
2022-10-01 03:35:13 +0200

    update action script

--- action_scripts/update_mage_version.sh
+++ action_scripts/update_mage_version.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
-# This script downloads the latest version number from xmage.de
+# This script downloads the latest beta version number from xmage.today
 # updates the VERSION file when it has changed
 # and creates a tag for the Version
 
-mage_version=$(curl --silent --show-error http://xmage.de/xmage/config.json | jq --raw-output '.XMage.version' |  cut -d " " -f1)
+mage_version=$(curl --silent --show-error http://xmage.today/config.json | jq --raw-output '.XMage.version' | sed 's/ /_/g' | sed 's/[\(\)]//g')
 echo $mage_version
 
 # At least try to check if it looks like a version number
-[[ $mage_version =~ ^[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+[[:digit:]_[:alnum:]]*$ ]] && echo "Looks like a version number" || exit 1 "Dont think this is a Version number"
+if [[ $mage_version =~ ^[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+-dev_.*$ ]]; then
+  echo "Looks like a version number"
+else
+  echo "Dont think this is a Version number"
+  exit 1 
+fi
 
 # Set mage version for other actions
 echo "MAGE_VERSION=$mage_version" >> $GITHUB_ENV
+
 # Update VERSION file
 echo $mage_version > XMAGE_VERSION
 
@@ -20,7 +26,7 @@ if [[ `git status --porcelain --untracked-files=no` ]]; then
   git config user.email "<>" && \
   git add XMAGE_VERSION && \
   git commit -m "update xmage version to ${mage_version}" && \
-  git tag -a ${mage_version} -m "XMage Release Version ${mage_version}" && \
+  git tag -a ${mage_version} -m "XMage Beta Version ${mage_version}" && \
   git push origin master && \
   git push origin ${mage_version} && \
   echo "\o/"
